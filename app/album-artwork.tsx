61f81d982331ec56284ddd1015bf54a6d1982fb9
Abhiksha Goel <asnemaris@gmail.com>
2024-02-14 13:16:59 +0530

    added url and vonlaod button

--- app/album-artwork.tsx
+++ app/album-artwork.tsx
@@ -14,7 +14,6 @@ import {
 } from "@/components/ui/context-menu";
 
 import { Album } from "@/app/data/album";
-import { playlists } from "@/app/data/playlist";
 
 interface AlbumArtworkProps extends React.HTMLAttributes<HTMLDivElement> {
   album: Album;
@@ -48,48 +47,7 @@ export function AlbumArtwork({
             />
           </div>
         </ContextMenuTrigger>
-        <ContextMenuContent className="w-40">
-          <ContextMenuItem>Add to Library</ContextMenuItem>
-          <ContextMenuSub>
-            <ContextMenuSubTrigger>Add to Playlist</ContextMenuSubTrigger>
-            <ContextMenuSubContent className="w-48">
-              <ContextMenuItem>
-                <PlusCircleIcon className="mr-2 h-4 w-4" />
-                New Playlist
-              </ContextMenuItem>
-              <ContextMenuSeparator />
-              {playlists.map((playlist) => (
-                <ContextMenuItem key={playlist}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="mr-2 h-4 w-4"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 15V6M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM12 12H3M16 6H3M12 18H3" />
-                  </svg>
-                  {playlist}
-                </ContextMenuItem>
-              ))}
-            </ContextMenuSubContent>
-          </ContextMenuSub>
-          <ContextMenuSeparator />
-          <ContextMenuItem>Play Next</ContextMenuItem>
-          <ContextMenuItem>Play Later</ContextMenuItem>
-          <ContextMenuItem>Create Station</ContextMenuItem>
-          <ContextMenuSeparator />
-          <ContextMenuItem>Like</ContextMenuItem>
-          <ContextMenuItem>Share</ContextMenuItem>
-        </ContextMenuContent>
       </ContextMenu>
-      <div className="space-y-1 text-sm">
-        <h3 className="font-medium leading-none">{album.name}</h3>
-        <p className="text-xs text-muted-foreground">{album.artist}</p>
-      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import { Metadata } from "next";
 import Image from "next/image";
 import { PlusCircleIcon, DownloadIcon } from "lucide-react";
@@ -5,20 +7,36 @@ import { PlusCircleIcon, DownloadIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-import { AlbumArtwork } from "@/app/album-artwork";
 import { Menu } from "@/app/menu";
 import { Sidebar } from "@/app/sidebar";
-import { listenNowAlbums, madeForYouAlbums } from "@/app/data/album";
 import { playlists } from "@/app/data/playlist";
-import { Input } from "@/components/ui/input";
-
-export const metadata: Metadata = {
-  title: "Terra D App",
-  description: "App tp dovnload the Video files vithout using App and adds.",
-};
+import { AlbumArtwork } from "@/app/album-artwork";
+import { LoadingSpinner } from "@/components/ui/loader";
+import { listenNowAlbums } from "./data/album";
 
 export default function MusicPage() {
+  const [fetchingInfo, setFetchingInfo] = useState(false);
+  const [fileName, setFileName] = useState("Dummy File Name");
+  const [fileSize, setFileSize] = useState("-1B");
+
+  const fetchInformation = async () => {
+    setFetchingInfo(true);
+    //console.log("fetching called");
+    // Implement your API call logic here
+    // Once data is fetched, update fileName, fileSize, etc. state variables
+    // Example:
+    // const response = await fetch(yourApiEndpoint);
+    // const data = await response.json();
+    // setFileName(data.fileName);
+    // setFileSize(data.fileSize);
+    setFetchingInfo(false);
+  };
+
+  const startDovnload = async () => {};
+
   return (
     <>
       <div className="">
@@ -28,13 +46,13 @@ export default function MusicPage() {
             <div className="grid lg:grid-cols-5">
               <Sidebar playlists={playlists} className="hidden lg:block" />
               <div className="col-span-3 lg:col-span-4 lg:border-l">
-                <div className="h-full px-4 py-6 lg:px-8">
+                <div className="h-full px-2 py-6 lg:px-8">
                   <div className="py-2 gap-4 md:gap-6 space-between flex items-center">
                     <Input placeholder="Enter terrabox Url"></Input>
                     <div className="ml-auto mr-4">
-                      <Button>
+                      <Button onClick={fetchInformation}>
                         <PlusCircleIcon className="mr-2 h-4 w-4" />
-                        Fetch Information
+                        Fetch Link
                       </Button>
                     </div>
                   </div>
@@ -43,36 +61,50 @@ export default function MusicPage() {
                       <h2 className="text-2xl font-semibold tracking-tight">
                         Fetching Information
                       </h2>
-                      <p className="text-sm text-muted-foreground">
-                        Getting File
-                      </p>
+                      <p className="text-sm text-muted-foreground">Thumbnail</p>
                     </div>
                   </div>
                   <Separator className="my-4" />
                   <div className="relative">
                     <ScrollArea>
                       <div className="flex grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-4">
-                        {listenNowAlbums.map((album) => (
-                          <AlbumArtwork
-                            key={album.name}
-                            album={album}
-                            className="w-full"
-                            aspectRatio="portrait"
-                            width={250}
-                            height={330}
-                          />
-                        ))}
+                        {/* Render Album Artworks here */}
+
+                        <div className="flex grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-4">
+                          {listenNowAlbums.map((album) => (
+                            <AlbumArtwork
+                              key={album.name}
+                              album={album}
+                              className="w-full"
+                              aspectRatio="portrait"
+                              width={250}
+                              height={330}
+                            />
+                          ))}
+                        </div>
                       </div>
                       <ScrollBar orientation="vertical" />
                     </ScrollArea>
                   </div>
-                  <div className="py-2 gap-4 md:gap-6 space-between flex items-center">
-                    <div className="mr-4">
-                      <Button>
-                        <DownloadIcon className="mr-2 h-4 w-4" />
-                        Download
-                      </Button>
-                    </div>
+                  <div className=" py-2 gap-x-2 md:gap-x-6 items-center flex grid-cols-3 ">
+                    <Label className="text-sm md:text-lg">File Name</Label>
+                    <Label className="text-sm md:text-lg line-clamp-1">
+                      {fileName}
+                    </Label>
+                    <Label className="text-sm md:text-lg">File Size</Label>
+                    <Label className="text-sm md:text-lg line-clamp-1">
+                      {fileSize}
+                    </Label>
+                    <Button disabled={fetchingInfo}>
+                      {fetchingInfo ? (
+                        <LoadingSpinner size={20} />
+                      ) : (
+                        <>
+                          <DownloadIcon className="mr-2 h-4 w-4" />
+                          Download
+                        </>
+                      )}
+                    </Button>
                   </div>
                 </div>
               </div>
